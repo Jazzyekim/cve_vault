13f83b1adc1728a156963be4268431f0128e6c5f
misha.dyrda <misha.dyrda@fiserv.com>
2024-09-04 14:46:13 +0200

    Move bulk insert to CVERepository

--- cve_data_sync_service/db_load.py
+++ cve_data_sync_service/db_load.py
@@ -2,22 +2,14 @@ import asyncio
 import json
 import os
 from datetime import datetime
+from typing import List
 
 import aiofiles
 
 from cve_data_sync_service import SYNC_CONFIG
+from db.cve_repository import make_cve, CVERepository
 from db.deps import get_db_session
 from db.models.cve import CVERecordDB
-
-
-def make_cve(cve_id: str, title: str, description: str, date_published: datetime,
-             date_updated: datetime) -> CVERecordDB:
-    return CVERecordDB(id=cve_id,
-                       title=title,
-                       description=description,
-                       date_published=date_published,
-                       date_updated=date_updated
-                       )
 
 
 class DbDataLoader:
@@ -25,7 +17,7 @@ class DbDataLoader:
         self.batch_size = batch_size
         pass
 
-    async def batch_data(self, file_path, cve_batch):
+    async def batch_data(self, file_path, cve_batch: List[CVERecordDB]):
         async with aiofiles.open(file_path, 'r', encoding='utf-8') as f:
             content = await f.read()
             data = json.loads(content)
@@ -42,12 +34,12 @@ class DbDataLoader:
 
                 date_published = datetime.fromisoformat(published_)
                 date_updated = datetime.fromisoformat(data['cveMetadata']['dateUpdated'])
-                cve = make_cve(cve_id, title, description, date_published, date_updated)
+                cve: CVERecordDB = make_cve(cve_id, title, description, date_published, date_updated)
 
                 cve_batch.append(cve)
 
     async def scan_directory(self, data_path=SYNC_CONFIG['data_dir']):
-        cve_batch = []
+        cve_batch: List[CVERecordDB] = []
 
         for dirpath, dirnames, filenames in os.walk(data_path):
             tasks = []
@@ -62,16 +54,15 @@ class DbDataLoader:
             if len(cve_batch) >= self.batch_size:
                 async for db in get_db_session():
                     async with db as session:
-                        session.add_all(cve_batch)
-                        await session.commit()
+                        await CVERepository(session).add_cve_batch(cve_batch)
                     cve_batch.clear()
 
         if cve_batch:
             async for db in get_db_session():
                 async with db as session:
-                    session.add_all(cve_batch)
-                    await session.commit()
+                    await CVERepository(session).add_cve_batch(cve_batch)
+
 
 
 if __name__ == '__main__':
-    asyncio.run( DbDataLoader(batch_size=100).scan_directory(data_path="cve_data/cves/2000"))
+    asyncio.run(DbDataLoader(batch_size=100).scan_directory(data_path="cve_data/cves/1999"))
--- db/cve_repository.py
+++ db/cve_repository.py
@@ -1,5 +1,5 @@
 from collections.abc import Sequence
-from typing import Annotated
+from typing import Annotated, List
 
 from db.models.cve import CVERecordDB
 from fastapi import Depends
@@ -9,6 +9,7 @@ from sqlalchemy.future import select
 from api_cve_service.schemas import CVERecord
 
 from db import deps
+from datetime import datetime
 
 
 class CVERepository:
@@ -33,6 +34,20 @@ class CVERepository:
         await self.db.refresh(cve_record_db)
         return cve_record_db
 
+    async def add_cve_batch(self, records: List[CVERecordDB]) -> None:
+        self.db.add_all(records)
+        await self.db.commit()
+
 
 async def get_cve_repository(db: Annotated[AsyncSession, Depends(deps.get_db_session)]) -> CVERepository:
     return CVERepository(db)
+
+
+def make_cve(cve_id: str, title: str, description: str, date_published: datetime,
+             date_updated: datetime) -> CVERecordDB:
+    return CVERecordDB(id=cve_id,
+                       title=title,
+                       description=description,
+                       date_published=date_published,
+                       date_updated=date_updated
+                       )
