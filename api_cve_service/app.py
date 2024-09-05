e12603d6b8ea31ade7acb1d2eabcaa736f18334a
misha.dyrda <misha.dyrda@fiserv.com>
2024-09-05 10:08:28 +0200

    Remove redundant lifespan

--- api_cve_service/app.py
+++ api_cve_service/app.py
@@ -1,22 +1,6 @@
-from contextlib import asynccontextmanager
-
 from fastapi import FastAPI
+
 from api_cve_service.routes.router import api_route
-from db.deps import get_engine
-from db.models import Base
 
-
-@asynccontextmanager
-async def lifespan(_app: FastAPI):
-    engine = get_engine()
-
-    async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
-
-    yield
-
-    pass
-
-
-app = FastAPI(title="CVE Vault", lifespan=lifespan, debug=True)
+app = FastAPI(title="CVE Vault", debug=True)
 app.include_router(api_route)
